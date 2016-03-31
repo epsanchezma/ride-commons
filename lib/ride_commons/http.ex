43a6d99790207de47efabdf8c2d12572dfd3faea
Guillermo Iguaran <guilleiguaran@gmail.com>
2016-03-31 15:31:07 -0500

    Allow the use of multiple HTTP backends with the http_module config option

--- lib/ride_commons/http.ex
+++ lib/ride_commons/http.ex
@@ -1,66 +1,67 @@
 defmodule RideCommons.HTTP do
   use Retry
 
+  @http_mod Application.get_env(:ride_commons, :http_module, HTTPoison)
   @retry_limit 3
   @retry_sleep 100
 
   def get(url, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.get(url, headers, options))
+      process_response(@http_mod.get(url, headers, options))
     end
   end
 
   def get!(url, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.get!(url, headers, options))
+      process_response(@http_mod.get!(url, headers, options))
     end
   end
 
   def post(url, body, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.post(url, body, headers, options))
+      process_response(@http_mod.post(url, body, headers, options))
     end
   end
 
   def post!(url, body, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response!(HTTPoison.post!(url, body, headers, options))
+      process_response!(@http_mod.post!(url, body, headers, options))
     end
   end
 
   def put(url, body, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.put(url, body, headers, options))
+      process_response(@http_mod.put(url, body, headers, options))
     end
   end
 
   def put!(url, body, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.put!(url, body, headers, options))
+      process_response(@http_mod.put!(url, body, headers, options))
     end
   end
 
   def patch(url, body, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.patch(url, body, headers, options))
+      process_response(@http_mod.patch(url, body, headers, options))
     end
   end
 
   def patch!(url, body, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response!(HTTPoison.patch!(url, body, headers, options))
+      process_response!(@http_mod.patch!(url, body, headers, options))
     end
   end
 
   def delete(url, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.delete(url, headers, options))
+      process_response(@http_mod.delete(url, headers, options))
     end
   end
 
   def delete!(url, headers, options \\ []) do
     retry @retry_limit in @retry_sleep do
-      process_response(HTTPoison.delete!(url, headers, options))
+      process_response(@http_mod.delete!(url, headers, options))
     end
   end
 
